0dd6fe8fc666382cfdeb8a78246f3dfe7463b4a5
Aondo-Chris <Chris@firmaflowledge.com>
2026-01-20 18:16:35 +0100

    edited home page

--- src/components/DefaultLayout.jsx
+++ src/components/DefaultLayout.jsx
@@ -6,7 +6,7 @@ const DefaultLayout = ({ header, children }) => {
       <header className="fixed top-0 w-full z-10">
         {header}
       </header>
-      <main className="flex-1 mt-5">
+      <main className="flex-1 mt-16">
         {children}
       </main>
     </div>
