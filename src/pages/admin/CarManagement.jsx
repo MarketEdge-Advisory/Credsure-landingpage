6d2d578d46509e445666276853c44da4f74c33b1
xpress-service <servixpress247@gamil.com>
2026-03-10 16:00:35 +0100

    fixed all errors

--- src/pages/admin/CarManagement.jsx
+++ src/pages/admin/CarManagement.jsx
@@ -535,6 +535,7 @@ const AddVehicleForm = ({ onBack, fetchVehicles }) => {
             <p className="text-sm text-gray-400 mt-0.5">Input the details below to add new vehicles</p>
           </div>
         </div>
+
         <div className="divide-y divide-gray-100">
           {/* Upload Images Row */}
           <div className="grid grid-cols-1 md:grid-cols-[200px_minmax(0,1fr)] gap-4 md:gap-8 py-6 w-full">
@@ -1163,8 +1164,7 @@ const CarManagement = () => {
       text: "You won't be able to revert this!",
       icon: 'warning',
       showCancelButton: true,
-      confirmButtonColor: '#d33',
-      confirmButtonColor:'#1e3f6e',
+      confirmButtonColor: '#1e3f6e',
       confirmButtonText: 'Yes, delete it!'
     });
 
